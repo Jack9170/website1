f6fcbc36dac9f77b257889dbbe11ae335a9a609b
Jack9170 <142847417+Jack9170@users.noreply.github.com>
2024-03-11 22:05:51 +0000

    Mon, Mar 11, 2024, 3:05 PM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,17 +1,27 @@
 toggle - document.getElementById('toggle')
 open = document.getElementById('open')
 modal = document.getElementById('modal')
+close = document.getElementById('close')
 
+// toggle modal
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
 
+//show modal
 open.addEventListener('click', () => {
     modal.classList.add('show-modal')
 })
 
 
-
+// hide modal
 close.addEventListener('click', () =>{
     modal.classList.remove('show-modal')
+})
+
+// Hide modal on outside click
+window.addEventListener('click', (e) => {
+    if (e.target == modal) {
+        modal.classList.remove('show-modal')
+    }
 })
